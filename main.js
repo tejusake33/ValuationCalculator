1637e865a7f8c922812ec5f0bf60cb3b4eaa51a7
Tejashree <sakharetejashree@gmail.com>
2022-06-10 12:13:52 +0530

    commit

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function calc() {
   var inves = document.getElementById("amount").value;
+  var inves = parseInt(inves,10);
   var eq = document.getElementById("equity").value;
-
+  var eq = parseInt(eq,10);
+  
   var num = inves*100;
   var deno = eq;
   var postmoney = num/deno;
