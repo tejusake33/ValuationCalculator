cf40f83b8723ba9f7ba2e4722b58173052238df5
Tejashree <sakharetejashree@gmail.com>
2022-06-17 15:10:18 +0530

    init

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ function calc() {
   document.getElementById("pre").value = premoney;
   document.getElementById("post").value = postmoney;
 }
+
+
+// function updateTextView(_obj){
+//   var num = getNumber(_obj.val());
+//   if(num==0){
+//     _obj.val('');
+//   }else{
+//     _obj.val(num.toLocaleString());
+//   }
+// }
+// function getNumber(_str){
+//   var arr = _str.split('');
+//   var out = new Array();
+//   for(var cnt=0;cnt<arr.length;cnt++){
+//     if(isNaN(arr[cnt])==false){
+//       out.push(arr[cnt]);
+//     }
+//   }
+//   return Number(out.join(''));
+// }
+// $(document).ready(function(){
+//   $('input[type=text]').on('keyup',function(){
+//     (updateTextView($(this)));
+//   });
+// });
